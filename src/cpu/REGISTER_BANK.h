3742d26ed59befd1d1e385ea0413e22106e23b44
Frank Leite <frankcefet090@gmail.com>
2024-10-21 18:09:40 -0300

    <feat>:<add ir register>

--- src/cpu/REGISTER_BANK.h
+++ src/cpu/REGISTER_BANK.h
@@ -14,7 +14,8 @@ using namespace std;
 struct REGISTER_BANK {
     //registradores de uso especifico
     REGISTER pc, mar, cr, epc, sr;                      // program counter, memory address register, cause register, exception program counter, status register
-    REGISTER hi, lo;                                    // registradores usados para armazenar os resultados de operações de multiplicação e divisão (resultado de 64 bits dividido em dois registradores de 32 bits)
+    REGISTER hi, lo; 
+    REGISTER ir;                                   // registradores usados para armazenar os resultados de operações de multiplicação e divisão (resultado de 64 bits dividido em dois registradores de 32 bits)
     //registradores de uso geral
 
     REGISTER zero;                                      // sempre é igual a 0
