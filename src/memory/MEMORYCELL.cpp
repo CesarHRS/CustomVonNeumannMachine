94796a57373454a8d3a0f9de5d76d5becb7689f7
João Gustavo <48455299+jAzz-hub@users.noreply.github.com>
2024-10-28 22:04:47 -0300

    <fix>: fixing the value constructor type

--- src/memory/MEMORYCELL.cpp
+++ src/memory/MEMORYCELL.cpp
@@ -1,14 +1,13 @@
-
 #include "MEMORYCELL.h"
 
 void MemoryCell::write(const uint32_t new_value) {
-    this->value.write(new_value);
+    this->value = new_value;
 }
 
-REGISTER MemoryCell::read() const{
+uint32_t MemoryCell::read() const{
     return value;
 }
 
-REGISTER MemoryCell::reverse_read() const{
+uint32_t MemoryCell::reverse_read() const{
     return value;
 }
