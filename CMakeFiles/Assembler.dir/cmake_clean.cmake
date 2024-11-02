9ced93ec2d71885a9466eec87be1bf038cc770d5
Getulio-Mendes <mendes.getulio@outlook.com>
2024-11-02 23:57:21 +0000

    Update cmake config

---
+++ CMakeFiles/Assembler.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Assembler"
+  "Assembler.pdb"
+  "CMakeFiles/Assembler.dir/assembler/assembler.cpp.o"
+  "CMakeFiles/Assembler.dir/assembler/assembler.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assembler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- CMakeFiles/Makefile.cmake
+++ CMakeFiles/Makefile.cmake
@@ -45,4 +45,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/CustomVonNeumannMachine.dir/DependInfo.cmake"
+  "CMakeFiles/Assembler.dir/DependInfo.cmake"
   )
