4535630539e45547da25f3ca13c84cb2e70c281f
Getulio-Mendes <mendes.getulio@outlook.com>
2024-11-02 10:04:02 +0000

    Control unit update

--- src/cpu/CONTROL_UNIT.h
+++ src/cpu/CONTROL_UNIT.h
@@ -10,54 +10,54 @@
 
 using namespace std;
 
-struct Temporary_Data{
-    string code_first_register;
-    string code_second_register;
-    string code_third_register;
-    string tarefa_a_ser_feita_pela_ula;
+struct Instruction_Data{
+    string source_register;
+    string target_register;
+    string destination_register;
+    string op;
     string addressRAMResult;
 };
 struct Control_Unit{
 
     operation op;
 
-    vector<Temporary_Data> data;
-
-    
-    const unordered_map<string, int> instructionMap = {
-        {"add", 0b0000},
-        {"and", 0b0001},
-        {"div", 0b0010},
-        {"mult", 0b0011},
-        {"sub", 0b0100},
-        {"beq", 0b0101},
-        {"bne", 0b0110},
-        {"bgt", 0b0111},
-        {"bgti", 0b1000},
-        {"blt", 0b1001},
-        {"blti", 0b1010},
-        {"j", 0b1011},
-        {"lw", 0b1100},
-        {"sw", 0b1101},
-        {"li", 0b1110},
-        {"la", 0b1111},
-        {"print", 0b10000}
+    vector<Instruction_Data> data;
+   
+    unordered_map<string, string> instructionMap = {
+        {"add", "000000"},
+        {"and", "000001"},
+        {"div", "000010"},
+        {"mult","000011"},
+        {"sub", "000100"},
+        {"beq", "000101"},
+        {"bne", "000110"},
+        {"bgt", "000111"},
+        {"bgti","001000"},
+        {"blt", "001001"},
+        {"blti","001010"},
+        {"j", "001011"},
+        {"lw", "001100"},
+        {"sw", "001101"},
+        {"li", "001110"},
+        {"la", "001111"},
+        {"print", "010000"}
     };
 
-    // principais operações de uma unidade de controle
-    string Pick_Adress_Result(const uint32_t instruction);
+    string Get_immediate(const uint32_t instruction);
     string Pick_Code_Register_Load(const uint32_t instruction);
-    string Pick_Third_Code_Register(const uint32_t instruction);
-    string Pick_Second_Code_Register(const uint32_t instruction);
-    string Pick_First_Code_Register(const uint32_t instruction);
+    string Get_destination_Register(const uint32_t instruction);
+    string Get_target_Register(const uint32_t instruction);
+    string Get_source_Register(const uint32_t instruction);
+
     string Identificacao_instrucao(const uint32_t instruction, REGISTER_BANK &registers);
     void Fetch(REGISTER_BANK &registers, bool &endProgram);
-    void Decode(REGISTER_BANK &registers, Temporary_Data &data);
-    void Execute_Aritmetic_Operation(REGISTER_BANK &registers,Temporary_Data &data);
-    void Execute_Loop_Operation(REGISTER_BANK &registers,Temporary_Data &data, int &counter); 
-    void Execute(REGISTER_BANK &registers, Temporary_Data &data, int &counter);
-    void Memory_Acess(REGISTER_BANK &registers,Temporary_Data &data);
-    void Write_Back(Temporary_Data &data);
+    void Decode(REGISTER_BANK &registers, Instruction_Data &data);
+    void Execute_Aritmetic_Operation(REGISTER_BANK &registers,Instruction_Data &data);
+    void Execute_Operation(REGISTER_BANK &registers,Instruction_Data &data);
+    void Execute_Loop_Operation(REGISTER_BANK &registers,Instruction_Data &data, int &counter); 
+    void Execute(REGISTER_BANK &registers, Instruction_Data &data, int &counter);
+    void Memory_Acess(REGISTER_BANK &registers,Instruction_Data &data);
+    void Write_Back(Instruction_Data &data);
 };
 
 #endif;
