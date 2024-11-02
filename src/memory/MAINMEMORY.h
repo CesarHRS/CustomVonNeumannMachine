182701ec558fbd655b43189508c253bd21e54416
João Gustavo <48455299+jAzz-hub@users.noreply.github.com>
2024-11-02 14:56:25 -0300

    <fix> :: fixing the implementation of MAINMEMORY.h | fixed segmentation fault

--- src/memory/MAINMEMORY.h
+++ src/memory/MAINMEMORY.h
@@ -1,3 +1,4 @@
+#ifndef MAINMEMORY_H
 #define MAINMEMORY_H
 
 #include "MEMORYCELL.h"
@@ -6,40 +7,39 @@
 using namespace std;
 
 struct MainMemory{
-        int NumOfj;
-        int NumOfi;
-        MemoryCell **words;
+	int NumOfj;
+	int NumOfi;
+	MemoryCell **words;
 
-        MainMemory() : NumOfj(2048), NumOfi(2048), words(NULL) {}
+	MainMemory() : NumOfj(2048), NumOfi(2048), words(NULL) {}
 
-        MainMemory(int NumOfj, int NumOfi, MemoryCell **word_size) {
+	MainMemory(int NumOfj, int NumOfi) {
 
-                this->NumOfi = NumOfi;
-                this->NumOfj = NumOfj;
-                words =  (MemoryCell**)malloc( sizeof(MemoryCell*) * this->NumOfi);
+		this->NumOfi = NumOfi;
+		this->NumOfj = NumOfj;
+		words =  (MemoryCell**)malloc( sizeof(MemoryCell*) * this->NumOfi);
 
-                for(uint32_t k = 0; k<NumOfi ; k++)
-                {
-                        words[k] = (MemoryCell*)malloc(sizeof(MemoryCell) * this->NumOfj);
+		for(uint32_t k = 0; k<NumOfi ; k++)
+		{
+			words[k] = (MemoryCell*)malloc(sizeof(MemoryCell) * this->NumOfj);
+		
+			
+		}
 
-
-                }
-
-                for(uint32_t i = 0 ; i < NumOfi ; i++ )
-                        for(uint32_t j = 0 ; j< NumOfj ; j++)
-                                words[i][j].write(0);
-
-        }
+		for(uint32_t i = 0 ; i < NumOfi ; i++ )
+			for(uint32_t j = 0 ; j< NumOfj ; j++)
+				words[i][j].write(0);
+		
+	}
 
         void InsertData(const uint32_t data, int iTarget, int jTarget);
-        void EraseData(int iTarget, int jTarget);
-        bool EmptyLine(int i) const;
-        uint32_t MainMemory::GetData(int iTarget, int jTarget) const;
+	void EraseData(int iTarget, int jTarget);
+	bool EmptyLine(int i) const;
+	void WriteMem(const uint32_t address, const uint32_t data);
+	const uint32_t ReadMem(const uint32_t address);
 
-        void WriteMem(const uint32_t address, const uint32_t data);
-        const uint32_t ReadMem(const uint32_t address);
-        
-//      void ShowBit(int NumOfj, int NumOfi);
-//      void WriteBit(REGISTER value, int iTarget, int jTarget);
+//	void ShowBit(int NumOfj, int NumOfi);
+//	void WriteBit(REGISTER value, int iTarget, int jTarget);
 
 };
+#endif
